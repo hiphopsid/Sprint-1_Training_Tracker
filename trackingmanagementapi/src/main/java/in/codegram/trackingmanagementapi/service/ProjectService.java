56493a1b4fc8eca737e0d6b82b893546c26afc56
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2022-03-29 13:15:18 +0530

    Delete trackingmanagementapi directory

--- trackingmanagementapi/src/main/java/in/codegram/trackingmanagementapi/service/ProjectService.java
+++
@@ -1,7 +0,0 @@
-package in.codegram.trackingmanagementapi.service;
-
-import in.codegram.trackingmanagementapi.domain.Project;
-
-public interface ProjectService {
-	public Project saveOrUpdate(Project project);
-}
